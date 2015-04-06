d09b16474cad6d494e834204a8a4171dbdf27523
loiphin <simonbonham@gmail.com>
2015-04-06 21:38:23 +0200

    Create init.lua

---
+++ init.lua
@@ -0,0 +1,21 @@
+
+--init.lua
+
+print("Setting up WIFI...")
+wifi.setmode(wifi.STATION)
+--modify according your wireless router settings
+wifi.sta.config("SSID","PASSWORD")
+wifi.sta.connect()
+tmr.alarm(1, 1000, 1, function() 
+if wifi.sta.getip()== nil then 
+print("IP unavaiable, Waiting...") 
+else 
+tmr.stop(1)
+print("Config done, IP is "..wifi.sta.getip())
+dofile("hcsr04.lua")
+device = hcsr04.init()
+tmr.alarm(0, 60000, 1, function() notify() end)
+end 
+end)
+
+
